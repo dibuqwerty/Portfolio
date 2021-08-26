a67ef48bfc325bbd2302f051d5420ca56c835d76
Piero David <piero_1997_12@hotmail.com>
2021-08-26 02:29:41 -0500

    javascript

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,12 @@ function myFunction(){
 function ventana(){
     window.alert("ah te la creiste")
 }
+document.getElementById('pro-left').addEventListener("click", function change(){
+    console.log('click');
+    document.getElementById("project-img").src = "multimedia/images/projects/project2.jpeg";
+});
+
+document.getElementById('pro-right').addEventListener("click", function change(){
+    console.log('click2');
+    document.getElementById("project-img").src = "multimedia/images/projects/project3.jpeg";
+});
