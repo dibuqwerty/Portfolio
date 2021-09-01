0f7be9db51ee6945908b680a3a3e78ce87421012
Piero David <piero_1997_12@hotmail.com>
2021-09-01 02:55:08 -0500

    changes

--- scripts/script.js
+++ scripts/script.js
@@ -12,6 +12,34 @@ document.getElementById('icon-cad').addEventListener("click", ()=>{
  
 });
 
+//ASIDE NAV
+window.onscroll = () => {
+    let scroll = document.documentElement.scrollTop;
+    if (scroll > 400) {
+      document.getElementById("show-nav").style.transform = "scale(1)";
+      console.log('transform 1');
+    } else {
+      document.getElementById("show-nav").style.transform = "scale(0)";
+      document.getElementById("span").classList.remove("menu-visible");
+      document.querySelector(".menu-icon").classList.remove("spin-menu");
+    }
+  };
+  // ANIMATION MENU
+document.querySelector(".show-menu").addEventListener("click", () => {
+    document.getElementById("span").classList.toggle("menu-visible");
+    document.querySelector(".menu-icon").classList.toggle("spin-menu");
+  });
+  
+  window.onload = () => {
+    // SHOW MENU
+    document.getElementById("show-nav").style.transform = "scale(0)";
+  
+    //LOADER ANIMATION
+   /* document.getElementById("loading").style.opacity = "0";
+    document.getElementById("loading").style.visibility = "hidden";*/
+    document.body.style.overflowY = "auto";
+    window.scrollTo(0, 0);
+  };
 
 ScrollReveal().reveal('.knowledge h1', {delay: 500});
 ScrollReveal().reveal('.about-me', {delay: 500});
